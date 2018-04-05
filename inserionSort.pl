fc47ea434871e7648eec82485f2f10a8742d8867
Ashiqur Rahman <ashiqur15-5470@diu.edu.bd>
2018-04-06 01:27:56 +0600

    query:  insertionsort([8,7,6,5,4,3,2,1],L).

--- inserionSort.pl
+++ inserionSort.pl
@@ -1,18 +1,4 @@
-% insertionSort([H | List], Result) :-
-%     insertionSort(List, Temp),
-%     printlist(Temp),
-%     insertItem(H, Temp, Result).
 
-% insertionSort([ ], [ ]).
-
-% insertItem(X, [H | List], [H | Result]) :-
-%     H < X, ! ,
-%     insertItem(X, List, [X | List]).
-
-% printlist([ ]) :- nl.
-%     printlist([X|List]):-
-%         write(X), write(" "),
-%         printlist(List).
 
 /* Insert sort */
 
